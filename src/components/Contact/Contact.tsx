31c1d78fd94983a235ea3a59d52b05d891c4f654
brianceasar <brianceasar7@gmail.com>
2025-06-24 16:17:04 +0300

    fix compile err

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -14,22 +14,22 @@ const containerVariants = {
   },
 };
 
-const itemVariants = {
-  hidden: { 
-    opacity: 0, 
-    y: 30,
-    scale: 0.95,
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-      ease: "easeOut",
-    },
-  },
-};
+// const itemVariants = {
+//   hidden: { 
+//     opacity: 0, 
+//     y: 30,
+//     scale: 0.95,
+//   },
+//   visible: {
+//     opacity: 1,
+//     y: 0,
+//     scale: 1,
+//     transition: {
+//       duration: 0.5,
+//       ease: "easeOut",
+//     },
+//   },
+// };
 
 const contactInfo = [
   {
